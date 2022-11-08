2ac3fb027749f34e95da8011ce68a3a59f8ffe99
Vinicius-klaus <vg222gy@student.lnu.se>
2022-11-08 15:03:28 -0300

    feat(login): load fonts before start the app

--- App.tsx
+++ App.tsx
@@ -1,13 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Login } from './src/pages';
+import AppLoading from 'expo-app-loading';
+import {
+  Inter_100Thin,
+  Inter_200ExtraLight,
+  Inter_300Light,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+  Inter_900Black, useFonts
+} from '@expo-google-fonts/inter';
 
 export default function App() {
-  return (
-    <Login/>
-  );
-}
+  let [fontsLoaded] = useFonts({
+    Inter_100Thin, 
+    Inter_200ExtraLight,
+    Inter_300Light,
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+    Inter_900Black 
+  })
 
+  if(!fontsLoaded){
+    return (
+      <AppLoading/>
+    );
+  }
+
+  return(
+    <Login/>
+  )
+
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -15,4 +45,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
